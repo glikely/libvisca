d95a5bdeaeaace7eb0747af987922efc6ef622d7
Damien Douxchamps <ddouxchamps@users.sf.net>
2019-08-12 15:52:57 +0900

    Various build system updates
    Signed-off-by: Damien Douxchamps <ddouxchamps@users.sf.net>

--- visca/libvisca_posix.c
+++ visca/libvisca_posix.c
@@ -54,9 +54,9 @@ _VISCA_write_packet_data(VISCAInterface_t *iface, VISCACamera_t *camera, VISCAPa
 
     err = write(iface->port_fd, packet->bytes, packet->length);
     if ( err < packet->length )
-	return VISCA_FAILURE;
+		return VISCA_FAILURE;
     else
-	return VISCA_SUCCESS;
+		return VISCA_SUCCESS;
 }
 
 
@@ -68,10 +68,10 @@ _VISCA_send_packet(VISCAInterface_t *iface, VISCACamera_t *camera, VISCAPacket_t
     if ((iface->address>7)||(camera->address>7)||(iface->broadcast>1))
     {
 #if DEBUG
-	fprintf(stderr,"(%s): Invalid header parameters\n",__FILE__);
-	fprintf(stderr," %d %d %d   \n",iface->address,camera->address,iface->broadcast);
+		fprintf(stderr,"(%s): Invalid header parameters\n",__FILE__);
+		fprintf(stderr," %d %d %d   \n",iface->address,camera->address,iface->broadcast);
 #endif
-	return VISCA_FAILURE;
+		return VISCA_FAILURE;
     }
 
     // build header:
@@ -79,11 +79,11 @@ _VISCA_send_packet(VISCAInterface_t *iface, VISCACamera_t *camera, VISCAPacket_t
     packet->bytes[0]|=(iface->address << 4);
     if (iface->broadcast>0)
     {
-	packet->bytes[0]|=(iface->broadcast << 3);
-	packet->bytes[0]&=0xF8;
+		packet->bytes[0]|=(iface->broadcast << 3);
+		packet->bytes[0]&=0xF8;
     }
     else
-	packet->bytes[0]|=camera->address;
+		packet->bytes[0]|=camera->address;
     
     // append footer
     _VISCA_append_byte(packet,VISCA_TERMINATOR);
@@ -101,16 +101,16 @@ _VISCA_get_packet(VISCAInterface_t *iface)
     // wait for message
     ioctl(iface->port_fd, FIONREAD, &(iface->bytes));
     while (iface->bytes==0) {
-	usleep(0);
-	ioctl(iface->port_fd, FIONREAD, &(iface->bytes));
+		usleep(0);
+		ioctl(iface->port_fd, FIONREAD, &(iface->bytes));
     }
 
     // get octets one by one
     bytes_read=read(iface->port_fd, iface->ibuf, 1);
     while (iface->ibuf[pos]!=VISCA_TERMINATOR) {
-	pos++;
-	bytes_read=read(iface->port_fd, &iface->ibuf[pos], 1);
-	usleep(0);
+		pos++;
+		bytes_read=read(iface->port_fd, &iface->ibuf[pos], 1);
+		usleep(0);
     }
     iface->bytes=pos+1;
 
@@ -126,86 +126,87 @@ _VISCA_get_packet(VISCAInterface_t *iface)
 uint32_t
 VISCA_open_serial(VISCAInterface_t *iface, const char *device_name)
 {
-  int fd;
-  fd = open(device_name, O_RDWR | O_NDELAY | O_NOCTTY);
+	int fd;
+	fd = open(device_name, O_RDWR | O_NDELAY | O_NOCTTY);
 
-  if (fd == -1)
+	if (fd == -1)
     {
 #if DEBUG
-      fprintf(stderr,"(%s): cannot open serial device %s\n",__FILE__,device_name);
+		fprintf(stderr,"(%s): cannot open serial device %s\n",__FILE__,device_name);
 #endif
-      iface->port_fd=-1;
-      return VISCA_FAILURE;
+		iface->port_fd=-1;
+		return VISCA_FAILURE;
     }	
-  else
+	else
     {
-      fcntl(fd, F_SETFL,0);
-      /* Setting port parameters */
-      tcgetattr(fd, &iface->options);
+		fcntl(fd, F_SETFL,0);
+		/* Setting port parameters */
+		tcgetattr(fd, &iface->options);
 
-      /* control flags */
-      cfsetispeed(&iface->options,B9600);    /* 9600 Bds   */
-      iface->options.c_cflag &= ~PARENB;     /* No parity  */
-      iface->options.c_cflag &= ~CSTOPB;     /*            */
-      iface->options.c_cflag &= ~CSIZE;      /* 8bit       */
-      iface->options.c_cflag |= CS8;         /*            */
-      iface->options.c_cflag &= ~CRTSCTS;    /* No hdw ctl */
+		/* control flags */
+		cfsetispeed(&iface->options,B9600);    /* 9600 Bds   */
+		iface->options.c_cflag &= ~PARENB;     /* No parity  */
+		iface->options.c_cflag &= ~CSTOPB;     /*            */
+		iface->options.c_cflag &= ~CSIZE;      /* 8bit       */
+		iface->options.c_cflag |= CS8;         /*            */
+		iface->options.c_cflag &= ~CRTSCTS;    /* No hdw ctl */
 
-      /* local flags */
-      iface->options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); /* raw input */
+		/* local flags */
+		iface->options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); /* raw input */
 
-      /* input flags */
-      /*
-      iface->options.c_iflag &= ~(INPCK | ISTRIP); // no parity
-      iface->options.c_iflag &= ~(IXON | IXOFF | IXANY); // no soft ctl
-      */
-      /* patch: bpflegin: set to 0 in order to avoid invalid pan/tilt return values */
-      iface->options.c_iflag = 0;
+		/* input flags */
+		/*
+		  iface->options.c_iflag &= ~(INPCK | ISTRIP); // no parity
+		  iface->options.c_iflag &= ~(IXON | IXOFF | IXANY); // no soft ctl
+		*/
+		/* patch: bpflegin: set to 0 in order to avoid invalid pan/tilt return values */
+		iface->options.c_iflag = 0;
 
-      /* output flags */
-      iface->options.c_oflag &= ~OPOST; /* raw output */
+		/* output flags */
+		iface->options.c_oflag &= ~OPOST; /* raw output */
 
-      tcsetattr(fd, TCSANOW, &iface->options);
+		tcsetattr(fd, TCSANOW, &iface->options);
 
     }
-  iface->port_fd = fd;
-  iface->address=0;
+	iface->port_fd = fd;
+	iface->address=0;
 
-  return VISCA_SUCCESS;
+	return VISCA_SUCCESS;
 }
 
 uint32_t
 VISCA_unread_bytes(VISCAInterface_t *iface, unsigned char *buffer, uint32_t *buffer_size)
 {
-  uint32_t bytes = 0;
-  *buffer_size = 0;
+	uint32_t bytes = 0;
+    int bytes_read;
+	*buffer_size = 0;
 
-  ioctl(iface->port_fd, FIONREAD, &bytes);
-  if (bytes>0)
+	ioctl(iface->port_fd, FIONREAD, &bytes);
+	if (bytes>0)
     {
-      bytes = (bytes>*buffer_size) ? *buffer_size : bytes;
-      read(iface->port_fd, &buffer, bytes);
-      *buffer_size = bytes;
-      return VISCA_FAILURE;
+		bytes = (bytes>*buffer_size) ? *buffer_size : bytes;
+		bytes_read=read(iface->port_fd, &buffer, bytes);
+		*buffer_size = bytes;
+		return VISCA_FAILURE;
     }
-  return VISCA_SUCCESS;
+	return VISCA_SUCCESS;
 }
 
 uint32_t
 VISCA_close_serial(VISCAInterface_t *iface)
 {
-  if (iface->port_fd!=-1)
+	if (iface->port_fd!=-1)
     {
-      close(iface->port_fd);
-      iface->port_fd = -1;
-      return VISCA_SUCCESS;
+		close(iface->port_fd);
+		iface->port_fd = -1;
+		return VISCA_SUCCESS;
     }
-  else
-    return VISCA_FAILURE;
+	else
+		return VISCA_FAILURE;
 }
 
 uint32_t
 VISCA_usleep(uint32_t useconds)
 {
-  return (uint32_t) usleep(useconds);
+	return (uint32_t) usleep(useconds);
 }
